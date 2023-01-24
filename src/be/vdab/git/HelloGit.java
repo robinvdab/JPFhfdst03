edad07fb477c16f208ed92c5c3280535bd571a2e
robin <robin@rbdgt.be>
2023-01-24 12:57:55 +0100

    Initial Commit

--- src/be/vdab/git/HelloGit.java
+++ src/be/vdab/git/HelloGit.java
@@ -2,6 +2,6 @@ package be.vdab.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git, test!");
     }
 }
